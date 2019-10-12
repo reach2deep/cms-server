8d07ef634fff37454afe6ba1896512f69108fde7
deep <reach2deep@gmail.com>
2019-10-12 18:53:08 +0800

    initial commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoUrl:'mongodb://localhost:27017/cms',
+    jwtSecret:'secret',
+    email : {
+        host:"smtp.mailtrap.io",
+        port:2525,
+        user:"064203f978c40d",
+        pass:"49f68f118f0ac0"
+    }
+}
